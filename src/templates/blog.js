1cade2745ecb82f915a550b45fe4e922f37c1da9
Vladimir <vladimir.skok@bvaccel.com>
2020-01-20 17:49:06 -0800

    fix: path to link component

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { graphql } from 'gatsby';
 import Img from 'gatsby-image';
 import Layout from '../components/layout';
-import Link from '../components/Link';
+import Link from '../components/link';
 
 const Blog = ({
   data: { allMdx },
