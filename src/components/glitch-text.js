46873c852c1cc361cdd4435b1b2fe3527a9afbfa
Vladimir <vladimir.skok@bvaccel.com>
2020-05-01 23:30:32 -0700

    feat: add text-shadow, position, clip-path and color configs for GlitchText

--- src/components/glitch-text.js
+++ src/components/glitch-text.js
@@ -1,71 +1,54 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui'
-import { keyframes } from '@emotion/core' 
+import { keyframes } from '@emotion/core'
+import getRandomInt from '../utils/get-random-int'
 
-const keyframes1 = {
-  '0%': { clipPath: `inset(4% 0 53% 0)` },
-  '5%': { clipPath: `inset(24% 0 41% 0)` },
-  '10%': { clipPath: `inset(91% 0 91% 0)` },
-  '15%': { clipPath: `inset(41% 0 5% 0)` },
-  '20%': { clipPath: `inset(70% 0 88% 0)` },
-  '25%': { clipPath: `inset(99% 0 22% 0)` },
-  '30%': { clipPath: `inset(1% 0 47% 0)` },
-  '35%': { clipPath: `inset(6% 0 93% 0)` },
-  '40%': { clipPath: `inset(61% 0 39% 0)` },
-  '45%': { clipPath: `inset(25% 0 45% 0)` },
-  '50%': { clipPath: `inset(30% 0 30% 0)` },
-  '55%': { clipPath: `inset(47% 0 29% 0)` },
-  '60%': { clipPath: `inset(99% 0 16% 0)` },
-  '65%': { clipPath: `inset(84% 0 29% 0)` },
-  '70%': { clipPath: `inset(25% 0 99% 0)` },
-  '75%': { clipPath: `inset(10% 0 73% 0)` },
-  '80%': { clipPath: `inset(92% 0 81% 0)` },
-  '85%': { clipPath: `inset(14% 0 21% 0)` },
-  '90%': { clipPath: `inset(34% 0 82% 0)` },
-  '95%': { clipPath: `inset(3% 0 29% 0)` },
-  '100%': { clipPath: `inset(33% 0 9% 0)` },
-}
-const keyframes2 = {
-  '0%': { clipPath: `inset(24% 0 3% 0)` },
-  '5%': { clipPath: `inset(4% 0 21% 0)` },
-  '10%': { clipPath: `inset(9% 0 1% 0)` },
-  '15%': { clipPath: `inset(61% 0 15% 0)` },
-  '20%': { clipPath: `inset(7% 0 8% 0)` },
-  '25%': { clipPath: `inset(9% 0 2% 0)` },
-  '30%': { clipPath: `inset(15% 0 17% 0)` },
-  '35%': { clipPath: `inset(56% 0 23% 0)` },
-  '40%': { clipPath: `inset(11% 0 29% 0)` },
-  '45%': { clipPath: `inset(15% 0 4% 0)` },
-  '50%': { clipPath: `inset(3% 0 3% 0)` },
-  '55%': { clipPath: `inset(7% 0 2% 0)` },
-  '60%': { clipPath: `inset(99% 0 1% 0)` },
-  '65%': { clipPath: `inset(14% 0 49% 0)` },
-  '70%': { clipPath: `inset(25% 0 49% 0)` },
-  '75%': { clipPath: `inset(10% 0 73% 0)` },
-  '80%': { clipPath: `inset(22% 0 11% 0)` },
-  '85%': { clipPath: `inset(14% 0 21% 0)` },
-  '90%': { clipPath: `inset(34% 0 22% 0)` },
-  '95%': { clipPath: `inset(3% 0 29% 0)` },
-  '100%': { clipPath: `inset(23% 0 9% 0)` },
+const ANIMATION_DURATION = `5000ms`
+const KEYFRAMES_NUMBER = 20
+const GLITCH_COLORS = [`red`, `green`, `blue`]
+
+const randClipPath = () => getRandomInt(1, 100) + `%`
+
+const randTextShadow = () => {
+  const shadow = getRandomInt(-1, 1)
+  return shadow === 0 ?
+    randPosition() :
+    shadow + `px`
 }
 
-// const rand = () => Math.floor(Math.random() * 100) + 1 + '%'
-// const clipPath = () => `inset(${rand()} 0 ${rand()} 0)`
-// const keys = Array.from({ length: 20 }).map((_, i) => i * 5 + '%')
+const randColor = () => GLITCH_COLORS[getRandomInt(0, 2)]
 
-// for (let i = 0; i < keys.length; i++) {
-//   keyframes1[keys[i]] = { clip: clipPath() }
-//   keyframes2[keys[i]] = { clip: clipPath() }
-// }
+const randPosition = () => {
+  const pos = getRandomInt(-5, 5)
+  return pos === 0 ?
+    randPosition() :
+    pos + `px`
+}
+
+const clipPath = () => `inset(${randClipPath()} 0 ${randClipPath()} 0)`
+const textShadow = () => `${randTextShadow()} 0 ${randColor()}`
+
+const keys = Array.from({ length: KEYFRAMES_NUMBER }).map((_, i) => i * 5 + '%')
+
+const keyframes1 = {}
+const keyframes2 = {}
+
+keys.forEach(key => {
+  keyframes1[key] = {
+    clipPath: clipPath(),
+    textShadow: textShadow(),
+    left: randPosition(),
+  }
+
+  keyframes2[key] = {
+    clipPath: clipPath(),
+    textShadow: textShadow(),
+    left: randPosition(),
+  }
+})
 
 const glitch1 = keyframes(keyframes1)
 const glitch2 = keyframes(keyframes2)
-
-// console.log('keys', keys)
-// console.log('keyframes1', keyframes1)
-// console.log('keyframes2', keyframes2)
-// console.log('glitch1', glitch1)
-// console.log('glitch2', glitch2)
 
 const GlitchText = ({ children, text }) => {
   return (
@@ -86,7 +69,7 @@ const GlitchText = ({ children, text }) => {
         backgroundColor: `background`,
         animationName: glitch1.toString(),
         animationTimingFunction: `linear`,
-        animationDuration: `3000ms`,
+        animationDuration: ANIMATION_DURATION,
         animationIterationCount: `infinite`,
       },
       '&::after': {
@@ -95,7 +78,7 @@ const GlitchText = ({ children, text }) => {
         backgroundColor: `background`,
         animationName: glitch2.toString(),
         animationTimingFunction: `linear`,
-        animationDuration: `3000ms`,
+        animationDuration: ANIMATION_DURATION,
         animationIterationCount: `infinite`,
       }
     }}>
