b6f94402fa85ab6b746329c21dc78f701b36aedb
Vladimir <vladimir.skok@bvaccel.com>
2020-05-02 23:14:13 -0700

    style: edit indentation  in GlitchText

--- src/components/glitch-text.js
+++ src/components/glitch-text.js
@@ -3,11 +3,15 @@ import { jsx } from 'theme-ui'
 import { keyframes } from '@emotion/core'
 import getRandomInt from '../utils/get-random-int'
 
-const maxClipPath = limit => 1 / (2 - (4 * (1 - limit)) ** 0.25) * 100
-const randClipPath = (maxClipPath, limit) => getRandomInt(1, maxClipPath(limit)) + `%`
+const maxClipPath = limit =>
+  1 / (2 - (4 * (1 - limit)) ** 0.25) * 100
+
+const randClipPath = (maxClipPath, limit) =>
+  getRandomInt(1, maxClipPath(limit)) + `%`
 
 const randTextShadow = (minShadow, maxShadow) => {
   const shadow = getRandomInt(minShadow, maxShadow)
+
   return shadow === 0 ?
     randPosition() :
     shadow + `px`
@@ -17,15 +21,23 @@ const randColor = colors => colors[getRandomInt(0, colors.length - 1)]
 
 const randPosition = (minPos, maxPos) => {
   const pos = getRandomInt(minPos, maxPos)
+
   return pos === 0 ?
     randPosition() :
     pos + `px`
 }
 
-const clipPath = (maxClipPath, limit) => `inset(${randClipPath(maxClipPath, limit)} 0 ${randClipPath(maxClipPath, limit)} 0)`
-const textShadow = (minShadow, maxShadow, colors) => `${randTextShadow(minShadow, maxShadow)} 0 ${randColor(colors)}`
+const clipPath = (maxClipPath, limit) => `inset(
+  ${randClipPath(maxClipPath, limit)} 0
+  ${randClipPath(maxClipPath, limit)} 0
+)`
 
-const keys = keyframesNum => Array.from({ length: keyframesNum + 1 }).map((_, i) => i * (100 / (keyframesNum + 1)) + '%')
+const textShadow = (minShadow, maxShadow, colors) => `
+  ${randTextShadow(minShadow, maxShadow)} 0 ${randColor(colors)}
+`
+
+const keys = keyframesNum => Array.from({ length: keyframesNum + 1 })
+  .map((_, i) => i * (100 / (keyframesNum + 1)) + '%')
 
 const getAnimation = (
   keyframesNum,
@@ -89,6 +101,7 @@ const GlitchText = ({
         '@media not screen and (prefers-reduced-motion: reduce)': {
           position: `relative`,
           display: `inline-block`,
+
           '&::before, &::after': {
             content: `"${text}"`,
             position: `absolute`,
@@ -97,6 +110,7 @@ const GlitchText = ({
             width: `100%`,
             height: `100%`,
           },
+
           '&::before': {
             backgroundColor,
             animationName: glitch1,
@@ -104,6 +118,7 @@ const GlitchText = ({
             animationDuration: duration,
             animationIterationCount: `infinite`,
           },
+
           '&::after': {
             backgroundColor,
             animationName: glitch2,
