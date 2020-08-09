cdc1697ae45b43e7819ca228294145150909822f
Vladimir Skok <skok@vova.io>
2020-08-08 20:34:05 -0700

    feat: edit Comment styles and add formatted time

--- src/components/Comment/index.js
+++ src/components/Comment/index.js
@@ -1,7 +1,7 @@
 /** @jsx jsx */
-import { jsx, Card, Styled } from "theme-ui"
+import { jsx, Card, Styled, Flex } from "theme-ui"
 import { useState } from "react"
-import { format } from "date-fns"
+// import { format } from "date-fns"
 
 const Comment = ({ to, src, alt, name, text, published }) => {
   const [highlight, setHighlight] = useState(false)
@@ -40,7 +40,6 @@ const Comment = ({ to, src, alt, name, text, published }) => {
           target="_blank"
           aria-label={`${name} twitter account avatar`}
           sx={{
-            display: `flex`,
             marginRight: `25px`,
           }}
         >
@@ -55,24 +54,31 @@ const Comment = ({ to, src, alt, name, text, published }) => {
           />
         </a>
 
-        <a
-          href={to}
-          rel="noopener noreferrer"
-          target="_blank"
-          aria-label={`${name} twitter account`}
-          sx={{
-            color: highlight ? `secondary` : `primary`,
-            margin: 0,
-            textDecoration: `none`,
-            transition: `color 400ms ease`,
-          }}
-        >
-          {name}
-        </a>
+        <Flex sx={{ flexDirection: `column` }}>
+          <a
+            href={to}
+            rel="noopener noreferrer"
+            target="_blank"
+            aria-label={`${name} twitter account`}
+            sx={{
+              color: highlight ? `secondary` : `primary`,
+              margin: 0,
+              textDecoration: `none`,
+              transition: `color 400ms ease`,
+            }}
+          >
+            {name}
+          </a>
 
-        <time dateTime={published}>
-          {format(new Date(published), `MM/dd/yyyy`)}
-        </time>
+          {/* <time
+            dateTime={published}
+            sx={{
+              fontSize: 1,
+            }}
+          >
+            {format(new Date(published), `MM/dd/yyyy`)}
+          </time> */}
+        </Flex>
       </div>
 
       <Styled.p
