609714f0bd5bc9c76a9968572aed85d98504883a
Vladimir Skok <skok@vova.io>
2020-08-13 23:09:55 -0600

    fix: merge conflict

--- src/utils/font-observer.js
+++ src/utils/font-observer.js
@@ -1,6 +1,6 @@
 const fontObserver = () => {
   if (sessionStorage.fontsLoaded) {
-    document.documentElement.classList.add(`fonts-stage-2`)
+    document.documentElement.classList.add(`font-loading-stage-2`)
     return
   } else {
     if (sessionStorage.fontsLoaded) {
