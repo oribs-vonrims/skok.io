af42bc9c4b6ff2e51d376cddeaf0345824df25a7
Vladimir Skok <skok@vova.io>
2020-08-28 19:47:24 -0400

    feat: add gatsby-source-firebase plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,7 @@
 const path = require("path")
 const siteMetadata = require("./site-metadata")
 
-const activeEnv = process.env.NODE_ENV
-
-require("dotenv").config({
-  path: `.env.${activeEnv}`,
-})
+require("dotenv").config({ path: `.env` })
 
 const gatsbyRemarkPlugins = [
   {
@@ -92,6 +88,35 @@ module.exports = {
         domain: `skok.club`,
         fetchLimit: 10000,
         token: process.env.WEBMENTION_TOKEN,
+      },
+    },
+    {
+      resolve: `@martinreiche/gatsby-firestore`,
+      options: {
+        appConfig: {
+          apiKey: process.env.FIREBASE_API_KEY,
+          authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+          databaseURL: process.env.FIREBASE_DATABASE_URL,
+          projectId: process.env.FIREBASE_PROJECT_ID,
+          storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+          messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+          appID: process.env.FIREBASE_APP_ID,
+          measurmentID: process.env.FIREBASE_MEASUREMENT_ID,
+        },
+        types: [
+          {
+            type: `Posts`,
+            collection: `posts`,
+            map: doc => ({ ...doc }),
+            subCollections: [
+              {
+                type: `Tweets`,
+                collection: `tweets`,
+                map: doc => ({ ...doc }),
+              },
+            ],
+          },
+        ],
       },
     },
     `gatsby-transformer-sharp`,
