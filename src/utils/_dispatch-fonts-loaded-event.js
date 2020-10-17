da308f57664b1d8fc91c77721a48f27a2cfd84e8
Vladimir Skok <skok@vova.io>
2020-10-16 23:47:51 -0400

    fix: remove old font loading code

--- src/utils/_dispatch-fonts-loaded-event.js
+++
@@ -1,9 +0,0 @@
-const dispatchFontsLoadEndEvent = () => {
-  document.documentElement.classList.add(`font-loading-stage-2`)
-
-  // Dispatch event to notify ThemeUIProvider component
-  const fontsLoadEndEvent = new CustomEvent(`fontsloadend`)
-  window.dispatchEvent(fontsLoadEndEvent)
-}
-
-export default dispatchFontsLoadEndEvent
