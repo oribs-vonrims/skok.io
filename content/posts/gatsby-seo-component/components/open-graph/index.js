81bc6b40185647b7897cdd2808cd742704966ce1
Vladimir Skok <skok@vova.io>
2021-08-18 02:20:06 -0400

    refactor: edit twitter and open graph components

--- content/posts/gatsby-seo-component/components/open-graph/index.js
+++ content/posts/gatsby-seo-component/components/open-graph/index.js
@@ -5,40 +5,16 @@ import CodeBlock from "../../../../../src/components/code-block"
 import Select from "../common/select"
 import { REGEX_PATTERN, SITE_METADATA } from "../common/constants"
 
-const {
+const { siteName, author, language, pages } = SITE_METADATA
+
+const composeData = ({ images, ...rest }) => ({
   siteName,
   author,
   language,
-  pages: { home, blog, contact, about },
-} = SITE_METADATA
-
-const pageMetadata = ({
-  type,
-  url,
-  title,
-  description,
-  image,
-  imageAlt,
-  published,
-  modified,
-}) => ({
-  type,
-  url,
-  title,
-  description,
-  image,
-  imageAlt,
-  published,
-  modified,
+  ...rest,
 })
 
-const initialState = {
-  siteName,
-  language,
-  ...pageMetadata(home),
-}
-
-const renderPreContent = ({
+const renderContent = ({
   isArticle,
   url,
   siteName,
@@ -51,8 +27,9 @@ const renderPreContent = ({
   modified,
   author,
 }) => {
-  const base = `
-    <meta property="og:type" content="${isArticle ? `article` : `website`}" />
+  const base = `<meta property="og:type" content="${
+    isArticle ? `article` : `website`
+  }" />
     <meta property="og:url" content="${url}" />
     <meta property="og:site_name" content="${siteName}" />
     <meta property="og:locale" content="${language}" />
@@ -61,60 +38,33 @@ const renderPreContent = ({
     <meta property="og:image" content="${image}" />
     <meta property="og:image:alt" content="${imageAlt}" />
   `
-
-  const article = `
-    <meta property="article:published_time" content="${published}" />
+  const article = `${base}<meta property="article:published_time" content="${published}" />
     <meta property="article:modified_time" content="${modified}" />
     <meta property="article:author" content="${author}" />
   `
+  const content = (isArticle ? article : base).replace(REGEX_PATTERN, ``)
 
-  return (isArticle ? `${base}${article}` : base).replace(REGEX_PATTERN, ``)
+  return content
 }
 
+const initialState = composeData(pages.home)
 const OpenGraphPlayground = () => {
   const [state, setState] = useState(initialState)
   const [selectedPage, setSelectedPage] = useState(`home`)
   const handlePageSelection = event => setSelectedPage(event.target.value)
 
   useEffect(() => {
-    if (selectedPage === `home`) {
-      setState(initialState)
-    }
-    if (selectedPage === `blog`) {
-      setState({
-        siteName,
-        language,
-        ...pageMetadata(blog),
-      })
-    }
-    if (selectedPage === `about`) {
-      setState({
-        siteName,
-        language,
-        ...pageMetadata(about),
-      })
-    }
-    if (selectedPage === `contact`) {
-      setState({
-        siteName,
-        language,
-        ...pageMetadata(contact),
-      })
-    }
-    if (selectedPage === `article`) {
-      setState({
-        isArticle: true,
-        siteName,
-        language,
-        author,
-      })
-    }
+    const page = composeData(pages[selectedPage])
+    setState({
+      ...page,
+      isArticle: selectedPage === `article`,
+    })
   }, [selectedPage])
 
   return (
     <Fragment>
       <Select onChange={handlePageSelection} />
-      <CodeBlock className="language-html">{renderPreContent(state)}</CodeBlock>
+      <CodeBlock className="language-html">{renderContent(state)}</CodeBlock>
     </Fragment>
   )
 }
--- content/posts/gatsby-seo-component/components/twitter/index.js
+++ content/posts/gatsby-seo-component/components/twitter/index.js
@@ -5,24 +5,17 @@ import CodeBlock from "../../../../../src/components/code-block"
 import Select from "../common/select"
 import { REGEX_PATTERN, SITE_METADATA } from "../common/constants"
 
-const {
-  creator,
-  pages: { home, blog, contact, about, article },
-} = SITE_METADATA
+const { creator, pages } = SITE_METADATA
 
-const pageMetadata = ({ title, description, image, imageAlt }) => ({
+const composeData = ({ title, description, image, imageAlt }) => ({
+  creator,
   title,
   description,
   image,
   imageAlt,
 })
 
-const initialState = {
-  creator,
-  ...pageMetadata(home),
-}
-
-const renderPreContent = ({ title, description, image, imageAlt, creator }) =>
+const renderContent = ({ title, description, image, imageAlt, creator }) =>
   `
   <meta name="twitter:card" content="summary_large_image" />
   <meta name="twitter:title" content="${title}" />
@@ -32,46 +25,18 @@ const renderPreContent = ({ title, description, image, imageAlt, creator }) =>
   <meta name="twitter:creator" content="${creator}" />
 `.replace(REGEX_PATTERN, ``)
 
+const initialState = composeData(pages.home)
 const TwitterPlayground = () => {
   const [state, setState] = useState(initialState)
   const [selectedPage, setSelectedPage] = useState(`home`)
-
   const handlePageSelection = event => setSelectedPage(event.target.value)
 
-  useEffect(() => {
-    if (selectedPage === `home`) {
-      setState(initialState)
-    }
-    if (selectedPage === `blog`) {
-      setState({
-        creator,
-        ...pageMetadata(blog),
-      })
-    }
-    if (selectedPage === `about`) {
-      setState({
-        creator,
-        ...pageMetadata(about),
-      })
-    }
-    if (selectedPage === `contact`) {
-      setState({
-        creator,
-        ...pageMetadata(contact),
-      })
-    }
-    if (selectedPage === `article`) {
-      setState({
-        creator,
-        ...pageMetadata(article),
-      })
-    }
-  }, [selectedPage])
+  useEffect(() => setState(composeData(pages[selectedPage])), [selectedPage])
 
   return (
     <Fragment>
       <Select onChange={handlePageSelection} />
-      <CodeBlock className="language-html">{renderPreContent(state)}</CodeBlock>
+      <CodeBlock className="language-html">{renderContent(state)}</CodeBlock>
     </Fragment>
   )
 }
