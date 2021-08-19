e4dc9646dc785e1c89cafef2e1d7687e9672e41a
Vladimir Skok <skok@vova.io>
2021-08-18 20:33:05 -0400

    feat: hide graphiql examples on mobile

--- content/components/gatsby-seo-series/graphiql-iframe/index.js
+++ content/components/gatsby-seo-series/graphiql-iframe/index.js
@@ -1,10 +1,26 @@
 /** @jsx jsx */
-import { jsx, Embed } from "theme-ui"
+import { Fragment } from "react"
+import { jsx, Embed, Box } from "theme-ui"
+import { useBreakpointIndex } from "@theme-ui/match-media"
 
 const GraphiQLIframe = ({
   title = `Gatsby GraphiQL playground`,
   url = `https://graphiql.linte.rs/___graphql?query=`,
   query,
-}) => <Embed title={title} src={`${url}${query}`} />
+}) => {
+  const isMobile = useBreakpointIndex() === 0
+
+  return isMobile ? (
+    <Fragment />
+  ) : (
+    <Box
+      sx={{
+        display: [`none`, `block`],
+      }}
+    >
+      <Embed title={title} src={`${url}${query}`} loading="lazy" />
+    </Box>
+  )
+}
 
 export default GraphiQLIframe
