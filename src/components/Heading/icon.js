e4f3cabbc177f183fc4951570788f383c1a4011f
Vladimir Skok <skok@vova.io>
2021-08-22 01:58:23 -0400

    fix: mobile heading id

--- src/components/Heading/icon.js
+++ src/components/Heading/icon.js
@@ -32,37 +32,36 @@ const Icon = ({ id, ariaLabel, threshold = 0.1, triggerOnce = true }) => {
 
   const isMobile = useBreakpointIndex() === 0
 
-  return isMobile ? (
-    <Fragment />
-  ) : (
-    <span ref={ref}>
-      <IconLink
-        target=""
-        rel=""
-        id={id}
-        href={`#${id}`}
-        aria-label={ariaLabel}
-        sx={{
-          scrollMarginTop: 3,
-          color: `primary`,
-          "&:hover": {
-            color: `secondary`,
-          },
-        }}
-      >
-        <motion.path
-          d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1"
+  return (
+    <span ref={ref} id={id}>
+      {!isMobile && (
+        <IconLink
+          target={false}
+          rel={false}
+          href={`#${id}`}
+          aria-label={ariaLabel}
           sx={{
-            strokeWidth: 2,
-            strokeLinecap: `round`,
-            strokeLinejoin: `round`,
-            fill: `none`,
-            stroke: `currentColor`,
+            scrollMarginTop: 3,
+            color: `primary`,
+            "&:hover": {
+              color: `secondary`,
+            },
           }}
-          animate={inView ? `visible` : `initial`}
-          variants={iconVariants}
-        />
-      </IconLink>
+        >
+          <motion.path
+            d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1"
+            sx={{
+              strokeWidth: 2,
+              strokeLinecap: `round`,
+              strokeLinejoin: `round`,
+              fill: `none`,
+              stroke: `currentColor`,
+            }}
+            animate={inView ? `visible` : `initial`}
+            variants={iconVariants}
+          />
+        </IconLink>
+      )}
     </span>
   )
 }
