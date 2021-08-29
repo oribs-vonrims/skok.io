18a7c618ae37b18da8aee4a745a80ba4365075dc
Vladimir Skok <skok@vova.io>
2021-08-29 15:40:53 -0400

    post: remove emoji from home page title

--- config/pages.js
+++ config/pages.js
@@ -2,7 +2,7 @@ const pages = {
   home: {
     id: `home`,
     pathName: `/`,
-    title: `Vladimir Skok 🧔`,
+    title: `Vladimir Skok`,
     description: `My name is Vladimir Skok. I move pixels on the web.`,
     image: `home.jpg`,
     imageAlt: `Home page cover photo`,
